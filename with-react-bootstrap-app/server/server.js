8153489054d5ad95439a8fb8867195dc248d218a
adam <adam2004buchan@outlook.com>
2025-03-04 14:29:36 +0000

    chnage from localhost to 127.0.0.1

--- with-react-bootstrap-app/server/server.js
+++ with-react-bootstrap-app/server/server.js
@@ -10,7 +10,7 @@ const client = new MongoClient(uri);
 app.use(express.json());
 
 app.use("/api/todolist", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:8000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
@@ -51,5 +51,5 @@ require("dotenv").config();
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
