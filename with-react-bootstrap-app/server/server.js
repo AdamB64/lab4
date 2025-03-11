f05b39a5db27027e164e222f08bc5f01f938c172
adam <adam2004buchan@outlook.com>
2025-03-11 13:31:05 +0000

    save db

--- with-react-bootstrap-app/server/server.js
+++ with-react-bootstrap-app/server/server.js
@@ -30,6 +30,7 @@ app.post("/api/todolist", async (req, res) => {
     const newTodo = { todoNumber: parseInt(req.query.todoNumber), todoText:req.query.todoText };
 
     const result = await collection.insertOne(newTodo);
+    await dbo.save();
     console.log("added "+JSON.stringify(result))
     } catch (error) {
       console.error(error);
